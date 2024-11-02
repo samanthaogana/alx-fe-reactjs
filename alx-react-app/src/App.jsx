ca3456f0a7ef1d19d1e6d5e6ba110bc003b9d19b
Samantha Ogana <samanthaogana@gmail.com>
2024-11-02 22:53:23 +0300

    Add code to file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -43,6 +44,9 @@ function App() {
     </div>
     <div>
       <Footer />
+    </div>
+    <div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </div>
     </>
   )
