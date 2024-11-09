8040a9b573b3cc40840b3b6c6cb3ce43aa41b9e8
Samantha Ogana <samanthaogana@gmail.com>
2024-11-09 22:58:22 +0300

    Add css inline styling

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav>
-        <ul style={{color: 'blue'}}>
+        <ul style={{color: 'blue', backgroundColor: 'ButtonShadow', display: 'flex', justifyContent: 'center', gap:'40px'}}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/services">Services</Link></li>
