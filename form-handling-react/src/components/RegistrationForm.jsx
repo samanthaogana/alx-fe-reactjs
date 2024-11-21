12fb69d141048bef91cd06d6c6617eb3adaf5568
Samantha Ogana <samanthaogana@gmail.com>
2024-11-21 17:26:54 +0300

    Add syntax to create a user registration form

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -13,4 +13,42 @@ const handleSubmit = (e) => {
         return;
     }
     console.log('Form submitted:', {username, email, password });
-}
+};
+
+return (
+    <form onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor="username">Username:</label>
+            <input 
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              />
+        </div>
+        <div>
+            <label htmlFor="email">Email:</label>
+            <input
+             type="email"
+             id="email"
+             value={email}
+             onChange={(e) => setEmail(e.target.value)}
+             required
+             />
+        </div>
+        <div>
+            <label htmlFor="password">Password:</label>
+            <input
+             type="password"
+             id="password"
+             value={password}
+             onChange={(e) => setPassword(e.target.value)}
+             required
+             />
+        </div>
+        <button type="submit">Register</button>
+    </form>
+);
+
+export default RegistrationForm;
