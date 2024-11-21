5b1790fe137fb06d48f20b4c8d09546a9facca09
Samantha Ogana <samanthaogana@gmail.com>
2024-11-21 17:37:35 +0300

    Correct code error

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,54 +1,55 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 
 const RegistrationForm = () => {
-    const[username, setUsername] = useState('');
-    const[email, setEmail] = useState('');
-    const[password, setPassword] = useState('');
-}
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if(username === '' || email === '' || password === '') {
-        alert('All forms are required');
-        return;
+    if (username === '' || email === '' || password === '') {
+      alert('All fields are required!');
+      return;
     }
-    console.log('Form submitted:', {username, email, password });
-};
+    
+    console.log('Form submitted:', { username, email, password });
+  };
 
-return (
+  return (
     <form onSubmit={handleSubmit}>
-        <div>
-            <label htmlFor="username">Username:</label>
-            <input 
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-              />
-        </div>
-        <div>
-            <label htmlFor="email">Email:</label>
-            <input
-             type="email"
-             id="email"
-             value={email}
-             onChange={(e) => setEmail(e.target.value)}
-             required
-             />
-        </div>
-        <div>
-            <label htmlFor="password">Password:</label>
-            <input
-             type="password"
-             id="password"
-             value={password}
-             onChange={(e) => setPassword(e.target.value)}
-             required
-             />
-        </div>
-        <button type="submit">Register</button>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Register</button>
     </form>
-);
+  );
+};
 
 export default RegistrationForm;
