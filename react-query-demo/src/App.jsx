33d930a68bc79cc16fb085719bfbc0129becc8cc
Samantha Ogana <samanthaogana@gmail.com>
2024-11-22 12:49:48 +0300

    import PostsComponent

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import PostsComponent from './components/PostsComponent'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -33,6 +34,9 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <PostsComponent />
       </QueryClientProvider>
+      <div>
+        <PostsComponent />
+      </div>
     </>
   )
 }
