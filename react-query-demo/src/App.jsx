b91d6c892932d24354bec5e2819fb93a379fcfac
Samantha Ogana <samanthaogana@gmail.com>
2024-11-22 12:13:52 +0300

    React Query Integration

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 function App() {
   const [count, setCount] = useState(0)
+  const queryClient = new QueryClient();
 
   return (
     <>
@@ -28,6 +30,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <QueryClientProvider client={queryClient}>
+        <PostsComponent />
+      </QueryClientProvider>
     </>
   )
 }
