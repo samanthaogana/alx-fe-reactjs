c1e639398ea2ff746fcf6ae1d305dfbbe5f576b9
Samantha Ogana <samanthaogana@gmail.com>
2024-11-22 13:07:04 +0300

    Modify error in file

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import PostsComponent from './components/PostsComponent'
 
 function App() {
@@ -34,9 +34,7 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <PostsComponent />
       </QueryClientProvider>
-      <div>
-        <PostsComponent />
-      </div>
+      
     </>
   )
 }
