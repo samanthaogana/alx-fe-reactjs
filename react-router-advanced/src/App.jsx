9be8d4ee9b689193066cec3bd5ac6b989466d9f9
Samantha Ogana <samanthaogana@gmail.com>
2024-11-23 21:07:22 +0300

    Dynamic Routing

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,6 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Profile from './components/Profile';
+import BlogPost from './components/BlogPost';
+
+
+const Home = () => { 
+  return <h2>Home Page</h2>; 
+};
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,6 +36,27 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Router>
+        <div>
+          <h1>Sam's Blog Post</h1>
+          <nav>
+            <Link to="/">Home</Link>
+            {' | '}
+            <Link to="/profile">Profile</Link>
+            {' | '}
+            <Link to="/posts">Posts</Link>
+            {' | '}
+            <Link to="/blog/1">Blog Post 1</Link>
+            {' | '}
+            <Link to="/blog2">Blog Post 2</Link>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile/*" element={<Profile />} />
+            <Route path="/blog/:postId" element={<BlogPost />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   )
 }
