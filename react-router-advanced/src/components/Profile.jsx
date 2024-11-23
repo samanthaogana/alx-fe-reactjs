ca13a376d1203cda871ada42daa070a50a754dbb
Samantha Ogana <samanthaogana@gmail.com>
2024-11-23 21:06:57 +0300

    Dynamic Routing

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Routes, Route } from react-router-dom;
+import { Link, Routes, Route } from 'react-router-dom';
 import ProfileDetails from "./ProfileDetails";
 import ProfileSettings from "./ProfileSettings";
 
