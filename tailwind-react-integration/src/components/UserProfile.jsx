e4590e673347368bf157a888797220e947571afd
Samantha Ogana <samanthaogana@gmail.com>
2024-11-30 15:34:37 +0300

    Modify error under img tag

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -17,9 +17,11 @@ function UserProfile() {
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-36 h-36 mx-auto sm:w-24 h-24 md:w-36 h-36"
+        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
       />
-      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">John Doe</h1>
+      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">
+        John Doe
+      </h1>
       <p className="text-gray-600 text-base sm:text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
