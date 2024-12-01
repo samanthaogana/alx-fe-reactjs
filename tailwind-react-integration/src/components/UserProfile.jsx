7c34e95cb085856cd87b29141a3820000b4163e7
Samantha Ogana <samanthaogana@gmail.com>
2024-12-01 15:24:10 +0300

    Advanced interactivity and transitions

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -13,13 +13,14 @@ function UserProfile() {
       md:p-8
       sm:max-w-xs
       md:max-w-sm
+      hover:shadow-xl
     >
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
+        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">
+      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-500">
         John Doe
       </h1>
       <p className="text-gray-600 text-base sm:text-sm md:text-base">
