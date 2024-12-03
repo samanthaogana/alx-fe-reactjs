1bb67734165a4e99c48b5c8d48cd654b0754d5dc
Samantha Ogana <samanthaogana@gmail.com>
2024-12-03 17:46:57 +0300

    import index.css file

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import "./index.css";
+import HomePage from "./components/HomePage";
 
 function App() {
   return <>
+  <div>
+    <HomePage />
+  </div>
   
-  
-  </>;
+  </>
 }
 
 export default App;
