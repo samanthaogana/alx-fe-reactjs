1da3b9e2c68c17fb2e8d20389ab1e94094476747
Samantha Ogana <samanthaogana@gmail.com>
2024-12-04 22:57:58 +0300

    Install react-router-dom

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "./App.css";
 import "./index.css";
 import HomePage from "./components/HomePage";
@@ -7,6 +8,12 @@ function App() {
   <div>
     <HomePage />
   </div>
+  <Router>
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/recipe/:id" element={<RecipeDetail />}></Route>
+    </Routes>
+  </Router>
   
   </>
 }
