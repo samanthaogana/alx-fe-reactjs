86c6ef658fd95b05b9ab5694b5e1ac64377ab314
Samantha Ogana <samanthaogana@gmail.com>
2024-12-05 10:38:02 +0300

    Add link to recipe.id

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 import recipeData from "../data.json";
 
 const HomePage = () => {
@@ -14,6 +15,7 @@ const HomePage = () => {
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {recipes.map((recipe) => (
           <div key={recipe.id} className="bg-white rounded-lg shadow-md p-4 hover:shadow-xl">
+            <Link to={`/recipe/${recipe.id}`}>
             <div className="flex justify-center items-center">
               <img
                 src={recipe.image}
@@ -21,9 +23,10 @@ const HomePage = () => {
                 className="rounded"
               />
             </div>
-
-            <h2 className="text-xl font-semibold mt-2">{recipe.title}</h2>
+            
+           <h2 className="text-xl font-semibold mt-2">{recipe.title}</h2>
             <p className="text-gray-600">{recipe.summary}</p>
+            </Link>
           </div>
         ))}
       </div>
