a34f11aa0116aaedd3cd5dce4b631e01c8dabfe7
Samantha Ogana <samanthaogana@gmail.com>
2024-12-06 12:12:27 +0300

    Import AddRecipeForm

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import "./index.css";
 import HomePage from "./components/HomePage";
 import RecipeDetail from './components/RecipeDetail';
+import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
   return <>
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
       <Route path="/recipe/:id" element={<RecipeDetail />}></Route>
+      <Route path="/add-recipe" element={<AddRecipeForm />}></Route>
     </Routes>
   </Router>
   
