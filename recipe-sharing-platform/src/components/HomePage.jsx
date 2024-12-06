02b327187d4e2dc1c5180dfcc78f606a9aa02ed3
Samantha Ogana <samanthaogana@gmail.com>
2024-12-06 12:14:29 +0300

    Link add recipe

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -12,6 +12,7 @@ const HomePage = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Recipe List</h1>
+      <Link to={"/add-recipe"}>Add New Recipe</Link>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {recipes.map((recipe) => (
           <div key={recipe.id} className="bg-white rounded-lg shadow-md p-4 hover:shadow-xl">
