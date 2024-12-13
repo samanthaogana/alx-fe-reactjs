3d3d6062fcaad61721759bb34425ca81691a322a
Samantha Ogana <samanthaogana@gmail.com>
2024-12-13 22:45:10 +0300

    Initial commit

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+<BrowserRouter>
+<Routes>
+  <Route>
+    
+  </Route>
+</Routes>
+</BrowserRouter>
+       </div>
+    </>
+  )
+}
+
+export default App
