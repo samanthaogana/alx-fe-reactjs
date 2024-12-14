0bb453dc066bc8b432fabdf60d91d567f0b450d0
Samantha Ogana <samanthaogana@gmail.com>
2024-12-14 22:28:20 +0300

    Advanced search functionality

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,15 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
-const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(
-      `https://api.github.com/users/${username}`
-    );
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching user data:", error);
-    throw error;
-  }
+const fetchUserData = async ({ username, location, minRepos }) => {
+    try {
+        const query = [];
+        if (username) query.push(`user:${username}`);
+        if (location) query.push(`location:${location}`);
+        if (minRepos) query.push(`repos:>${minRepos}`);
+
+        const queryString = query.join('+');
+        const response = await axios.get(`https://api.github.com/search/users?q=${queryString}`);
+        return response.data.items;
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        throw error;
+    }
 };
 
 export default fetchUserData;
